33558df9635a41422e63d11b4a2e9e260745013a
Karthik Sagar <karthiksagar@usc-guestwireless-upc-newsc15911.usc.edu>
2019-09-24 00:28:58 -0700

    Updates

---
+++ precache-manifest.28d1e96f90ee8174521ee66d4eb9a304.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f20f41508df3f9bde5aa15489d05eec",
+    "url": "/supersquad/index.html"
+  },
+  {
+    "revision": "6cd7f42fd6108c9e9358",
+    "url": "/supersquad/static/css/main.abb5ab7f.chunk.css"
+  },
+  {
+    "revision": "f00e2989d78828e90bf4",
+    "url": "/supersquad/static/js/2.73d02b3d.chunk.js"
+  },
+  {
+    "revision": "6cd7f42fd6108c9e9358",
+    "url": "/supersquad/static/js/main.6f7e53c7.chunk.js"
+  },
+  {
+    "revision": "d132b4573629358daa44",
+    "url": "/supersquad/static/js/runtime-main.63cc95c2.js"
+  },
+  {
+    "revision": "4ddc758d490102bf391845e6f0791725",
+    "url": "/supersquad/static/media/background.4ddc758d.jpg"
+  }
+]);
